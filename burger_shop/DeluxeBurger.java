121a798adcfeb6dc04c88aff3f441de751b0e0d7
Abigail Swigert <abigail@swigertdesigns.com>
2020-07-20 14:56:09 -0500

    Abigail swigert (#1)
    * most of ex.1 working, HealthBurger inncomplete
    
    * completed ex.1
    
    * exercise 2 meal class complete
    
    * meal now lists all sides
    
    * exercise 3 almost complete, need to limit quanity of each topping, and keep other classes from being instantiated
    
    * minor updates made

--- burger_shop/DeluxeBurger.java
+++ burger_shop/DeluxeBurger.java
@@ -1,4 +1,11 @@
 package burger_shop;
 
-public class DeluxeBurger {
+public class DeluxeBurger extends Burger {
+
+    public DeluxeBurger(Meat meat, Bread breadRollType) {
+        super(meat, breadRollType);
+        this.setBurgerName("Deluxe");
+        this.setBurgerPrice(4.99);
+        this.setMaxToppings(6);
+    }
 }
